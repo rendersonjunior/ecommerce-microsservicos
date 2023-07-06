e40763c2290d97240af0de823d5c334df9a8833b
Renderson Junior <rendersonjuniorcmp@gmail.com>
2023-07-05 23:13:43 -0300

    Commit inicial product-api

---
+++ ecommerce-product-api/src/main/java/br/com/rendersonjunior/ecommerceproductapi/EcommerceproductapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rendersonjunior.ecommerceproductapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceproductapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceproductapiApplication.class, args);
+	}
+
+}
---
+++ ecommerce-product-api/src/test/java/br/com/rendersonjunior/ecommerceproductapi/EcommerceproductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rendersonjunior.ecommerceproductapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceproductapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
