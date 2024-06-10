d71ebe89d41f9fd215e63bfd407a98644fea6cff
Renderson Junior <rendersonjuniorcmp@gmail.com>
2024-06-09 23:52:18 -0300

    feat: adicionando o Builder na classe

--- ecommerce-product-api/src/main/java/br/com/rendersonjunior/ecommerceproductapi/model/Product.java
+++ ecommerce-product-api/src/main/java/br/com/rendersonjunior/ecommerceproductapi/model/Product.java
@@ -13,6 +13,7 @@ import jakarta.persistence.PreUpdate;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
