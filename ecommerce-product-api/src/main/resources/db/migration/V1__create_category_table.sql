af09c316945cb7830aa053b45eeb74ad03175ff9
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-08-04 23:17:47 -0300

    fix: correcao nos creates tables do postgres

--- ecommerce-product-api/src/main/resources/db/migration/V1__create_category_table.sql
+++ ecommerce-product-api/src/main/resources/db/migration/V1__create_category_table.sql
@@ -1,7 +1,7 @@
 create schema if not exists products;
 
 create table products.category (
-   id int auto_increment primary key,
+   id bigint primary key,
    nome varchar(100) not null
 );
 
--- ecommerce-shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
+++ ecommerce-shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
@@ -1,14 +1,14 @@
 create schema if not exists shopping;
 
 create table shopping.shop (
-    id int auto_increment primary key,
+    id bigint primary key,
     user_identifier varchar(100),
     date timestamp not null,
     total numeric(20,2) not null
 );
 
 create table shopping.item (
-    shop_id int auto_increment references shopping.shop(id),
+    shop_id bigint references shopping.shop(id),
     product_identifier varchar(100) not null,
     preco numeric(20,2) not null
 );
