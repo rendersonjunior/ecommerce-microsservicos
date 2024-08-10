6fdd4136ab361060550dccda4b4916587dff9612
rendersonjunior <rendersonjuniorcmp@gmail.com>
2024-08-10 00:00:33 -0300

    fix: ajuste na sequence da entidade shop

--- ecommerce-shopping-api/src/main/java/br/com/rendersonjunior/ecommerceshoppingapi/model/Shop.java
+++ ecommerce-shopping-api/src/main/java/br/com/rendersonjunior/ecommerceshoppingapi/model/Shop.java
@@ -1,16 +1,7 @@
 package br.com.rendersonjunior.ecommerceshoppingapi.model;
 
 
-import jakarta.persistence.CollectionTable;
-import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,7 +20,8 @@ import java.util.List;
 public class Shop implements Comparable<Shop> {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sq_shopping")
+    @SequenceGenerator(name = "sq_shopping", sequenceName = "sq_shopping", initialValue = 1, allocationSize = 1)
     @Column(name = "id")
     private long id;
 
